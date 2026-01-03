213a21f0501773e2898e758a12699385f7bd6307
Greg Cheong <greg@gregcheong.com>
2026-01-03 09:26:38 -0800

    add ability to publish on github

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "/my_static_site/"
--- src/gencontent.py
+++ src/gencontent.py
@@ -2,7 +2,7 @@ import os
 from markdown_blocks import markdown_to_html_node
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath,from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -18,6 +18,7 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown_content)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', 'href="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -25,16 +26,16 @@ def generate_page(from_path, template_path, dest_path):
     to_file = open(dest_path, "w")
     to_file.write(template)
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(basepath, dir_path_content, template_path, dest_dir_path):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             if filename.endswith(".md"):
                 dest_path = dest_path[:-3] + ".html"
-                generate_page(from_path, template_path, dest_path)
+                generate_page(basepath, from_path, template_path, dest_path)
         else:
-            generate_page_recursive(from_path, template_path, dest_path)
+            generate_page_recursive(basepath, from_path, template_path, dest_path)
 
 def extract_title(md):
     lines = md.split("\n")
